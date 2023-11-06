8370b5e0c1b381c407ba3ef8af81fded610d294f
esarneso <emily.ecoff@gmail.com>
2023-11-06 13:41:04 -0500

    update version

--- adscore/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-import os
-
-
-class AdscoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adscore'
-
-    ADSCORE_NVD_API_KEY = os.environ.get('NVD_API_KEY')
